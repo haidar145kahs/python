b5fa029bf2a69793d0932bd1eb7cdd8031359846
haidar145kahs <haidar.kahs145@gmail.com>
2021-09-18 07:00:32 +0300

    'solved'

--- blabla.py
+++ blabla.py
@@ -1,2 +1,12 @@
-tasks = input().split(' ')
-print(int(tasks))
+k = int(input())
+string = input()
+d = dict()
+s =set()
+for i in string:
+    if i in s:
+        d[i]+=1
+    else:
+        s.add(i)
+        d[i]=1
+print(s)
+print(d)
--- k-String.py
+++ k-String.py
@@ -1,2 +1,30 @@
+k = int(input())
 string = input()
-#hello
+d = dict()
+s =set()
+arranged = ''
+flag=False
+if len(string)%k == 0 :
+    for i in string:
+        if i in s:
+            d[i]+=1
+        else:
+            s.add(i)
+            d[i]=1
+
+    for i in s:
+        if d[i]%k == 0 :
+            for j in range(int(d[i]/k)):
+                arranged+=i
+        else:
+            flag = True
+            break
+    if flag:
+        print('-1')
+    else:
+        ans = ''
+        for i in range(k):
+            ans+=arranged
+        print(ans)    
+else:
+    print('-1')
